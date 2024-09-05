1c7b81dd03f1aac55838bbc66fde8da5c05dbf6a
Helloa <160602656+nhelloa@users.noreply.github.com>
2024-09-05 10:45:10 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+print("""
+      
+Ｘ－ｓｐｏｒｔ
+      
+      """)
+
+print("1. Cadastro de alunos")
+print("2. Listar alunos")
+print("3. Ativar alunos")
+print("4. Salvar da aplicação")
+
+opcao_escolhida = int(input("Escolha uma opção; "))
+print("Você escolheu a opção; ", opcao_escolhida)
+
